620138970ed921d6f2caec744a3623c62d326fa3
Chris Muller <chris@pango.world>
2016-09-08 11:52:53 +0200

    Chris Build 3

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -15,6 +15,10 @@ class UnitTests extends \PHPUnit_Framework_TestCase{
 	}
 
 	public function test_wcct_get_option () {
+		$get_option = $this->getFunctionMock('WCCT16','get_option');
+		$get_option->expects($this->once())
+			->with($this->equalTo('wcct16_venue'))
+			->willReturn('river-club');
 		$this->assertEquals('river-club',wcct_get_option('venue'));
 	}
 }
